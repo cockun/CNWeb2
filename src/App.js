8f57b89af12239ff4e2f2824ee18a9e950f3f6d0
Best Nhái <thengansax612@gmail.com>
2020-06-22 19:26:58 +0700

    d

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 12321 213
-          213 213
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
