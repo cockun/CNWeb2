85f02909773311811f2c9e54e0e9781d8eee8326
Best Nhái <thengansax612@gmail.com>
2020-06-22 20:12:37 +0700

    d

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Footer from './Components/Footer';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Footer />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
