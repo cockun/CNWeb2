14b680f2bd09759bf7d50d17c5e86b9171617d50
Nhai <thengansax612@gmail.com>
2020-06-25 11:49:08 +0700

    dd

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -5,7 +5,46 @@ import Header from './Header';
 import Footer from './Footer'
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            data:[],
+            name :'',
+            pass:'',
+            
+        }
+        
+        this.check = this.check.bind(this);
+        
+      }
+      
+      async check() {
+        let resp = await fetch(
+          'https://5ee5aa77ddcea00016a37721.mockapi.io/Account'
+        );
+        let respJson = await resp.json();
+        this.setState({ data: respJson });
+        let a = parseInt(respJson.length);
+        let b = 0;
+    
+        for (var i = 0; i < a; i++) {
+          if (
+            String(respJson[i].name) == String(this.state.name) &&
+            String(respJson[i].password) == String(this.state.pass)
+          ) {
+            alert('Đăng nhập thành công');
+            break;
+          }
+          if (i == a - 1) {
+            alert('Sai tài khoản hoặc mật khẩu');
+            break;
+          }
+        }
+      }
+    
+    
   render() {
+    
     return (
       <div>
           <div>
@@ -18,15 +57,25 @@ export default class Login extends React.Component {
             <div className="loginform">
                 <div className="collogin">
                     <div className="login-form">
-                        <h2>Login 213</h2>
-                        <form action="#" className="form">
+                        <h2>Login</h2>
+                        <form action="#" className="form" onSubmit={this.check}>
                             <div className="group-input">
                                 <label for="username">Username or email address *</label>
-                                <input type="text" id="username"/>
+                                <input type="text" id="username" value={String(this.state.name)} 
+                                onChange={ (txt) => {
+                                  this.setState({name:txt.target.value})
+                                  
+                                } } />
                             </div>
                             <div className="group-input">
                                 <label for="pass">Password *</label>
-                                <input type="text" id="pass"/>
+                                <input type="text" id="pass" value={String(this.state.pass)} 
+                                onChange={ (txt) => {
+                                  this.setState({pass:txt.target.value})
+                                  
+                                } }
+                                
+                                />
                             </div>
                             <div className="group-input gi-check">
                                 <div className="gi-more">
