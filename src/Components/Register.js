29aa00e894368b7cf3b315c741b09dd0b8ec4c28
Nhai <thengansax612@gmail.com>
2020-06-25 11:51:32 +0700

    d

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import '../Css/Register.css';
 import Header from './Header';
-import Footer from './Footer'
+import Footer from './Footer';
+import {callApi} from '../utils/apiCaller';
 
 export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+        data:[],
+        name :'',
+        pass:'',
+        pass2:'',
+        fullname:'',
+        phone:'',
+        address:''  
+    }
+    
+    this.add = this.add.bind(this);
+    
+  }
+  async add() {
+    if (this.state.name !== '' && this.state.pass !== '') {
+      let resp = await fetch(
+        'https://5ee5aa77ddcea00016a37721.mockapi.io/Account'
+      );
+      let respJson = await resp.json();
+
+      this.setState({ data: respJson });
+
+      let a = parseInt(respJson.length);
+      let jus = 1;
+      for (var i = 0; i < a; i++) {
+        if (String(this.state.name) == String(respJson[i].name)) {
+          alert('Tên tài khoản đã tồn tại');
+          jus = 0;
+          break;
+        }
+      }
+      if (jus === 1) {
+        if (String(this.state.pass) == String(this.state.pass2)) {
+          callApi('Account', 'POST', {
+            id: '',
+            name: this.state.name,
+            password: this.state.pass,
+            fullname:this.state.fullname,
+            phone : this.state.phone,
+            address:this.state.address
+          });
+          alert('Đăng ký thành công');
+       
+        }
+        else
+        {
+          if (String(this.state.pass) != String(this.state.pass2)) {
+            alert('Mật khẩu nhập lại không đúng');
+          }
+        }
+      }     
+    }
+    else
+    {
+      alert('Vui lòng nhập đầy đủ thông tin!!!');
+    }
+  }
   render() {
     return (
       <div>
@@ -17,18 +77,54 @@ export default class Register extends React.Component {
                 <div class="colregister">
                     <div class="register-form">
                         <h2>Register</h2>
-                    <form action="#">
+                    <form action="#" onSubmit={this.add}>
                             <div class="group-input">
-                                <label for="username">Username or email address *</label>
-                                <input type="text" id="username"/>
+                                <label for="username">Tài khoản</label>
+                                <input type="text" id="username" value={String(this.state.name)} 
+                                onChange={ (txt) => {
+                                  this.setState({name:txt.target.value})
+                                  
+                                } } />
                             </div>
                             <div class="group-input">
-                                <label for="pass">Password *</label>
-                                <input type="text" id="pass"/>
+                                <label for="pass">Mật Khẩu </label>
+                                <input type="text" id="username" value={String(this.state.pass)} 
+                                onChange={ (txt) => {
+                                  this.setState({pass:txt.target.value})
+                                  
+                                } } />
                             </div>
                             <div class="group-input">
-                                <label for="con-pass">Confirm Password *</label>
-                                <input type="text" id="con-pass"/>
+                                <label for="con-pass">Nhập lại mật khẩu</label>
+                                <input type="text" id="username" value={String(this.state.pass2)} 
+                                onChange={ (txt) => {
+                                  this.setState({pass2:txt.target.value})
+                                  
+                                } } />
+                            </div>
+                            <div class="group-input">
+                                <label for="con-pass">Họ và tên</label>
+                                <input type="text" id="username" value={String(this.state.fullname)} 
+                                onChange={ (txt) => {
+                                  this.setState({fullname:txt.target.value})
+                                  
+                                } } />
+                            </div>
+                            <div class="group-input">
+                                <label for="con-pass">SĐT</label>
+                                <input type="text" id="username" value={String(this.state.phone)} 
+                                onChange={ (txt) => {
+                                  this.setState({phone:txt.target.value})
+                                  
+                                } } />
+                            </div>
+                            <div class="group-input">
+                                <label for="con-pass">Địa chỉ</label>
+                                <input type="text" id="username" value={String(this.state.address)} 
+                                onChange={ (txt) => {
+                                  this.setState({address:txt.target.value})
+                                  
+                                } } />
                             </div>
                             <button type="submit" class="site-btn register-btn">REGISTER</button>
                     </form>
