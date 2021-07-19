61209dc5be4d88fbc38b1c9e826394e1edc93b95
Vu Minh Khoa <vuminhkhoa97@gmail.com>
2021-07-19 17:43:04 +0700

    up

--- src/utils/apiCaller.js
+++ src/utils/apiCaller.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://35.247.162.235/api";
+//const API_URL = "http://35.247.162.235/api";
+const API_URL = "http://localhost:3000/api";
 
 export async function callApi(endpoint, method = "GET", body) {
   if(method ==="GET"){
