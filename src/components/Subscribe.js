a4976e221f358f907624ea081e7e194a9c04cb4a
Master Nhai <thengansax612@gmail.com>
2021-07-20 22:06:37 +0700

    d

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -3,29 +3,68 @@ import React from "react";
 export default function Subscribe() {
   return (
     <div id="mc_embed_signup">
-    <form action="https://gmail.us6.list-manage.com/subscribe/post?u=86965ed020b63caf48b05c566&amp;id=f534f52ed1"
-      method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank"
-      novalidate>
-      <div id="mc_embed_signup_scroll">
-        <h2>Subscribe</h2>
-        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-        <div class="mc-field-group">
-          <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
-          </label>
-          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL"/>
+      <form
+        action="https://gmail.us6.list-manage.com/subscribe/post?u=86965ed020b63caf48b05c566&amp;id=f534f52ed1"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+        noValidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <h2>Đăng ký nhận khuyến mãi</h2>
+          <div className="indicates-required">
+            <span className="asterisk">*</span> indicates required
+          </div>
+          <div className="mc-field-group">
+            <label htmlFor="mce-EMAIL">
+              Email Address <span className="asterisk">*</span>
+            </label>
+            <input
+              type="email"
+              defaultValue
+              name="EMAIL"
+              className="required email"
+              id="mce-EMAIL"
+            />
+          </div>
+          <div id="mce-responses" className="clear">
+            <div
+              className="response"
+              id="mce-error-response"
+              style={{ display: "none" }}
+            />
+            <div
+              className="response"
+              id="mce-success-response"
+              style={{ display: "none" }}
+            />
+          </div>{" "}
+          {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
+          <div
+            style={{ position: "absolute", left: "-5000px" }}
+            aria-hidden="true"
+          >
+            <input
+              type="text"
+              name="b_7bd56446690088a5528dcb07e_0d9bb024c2"
+              tabIndex={-1}
+              defaultValue
+            />
+          </div>
+          <div className="clear">
+            <input
+              type="submit"
+              defaultValue="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className="button"
+            />
+          </div>
         </div>
-        <div id="mce-responses" class="clear">
-          <div class="response" id="mce-error-response" style="display:none"></div>
-          <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>
-        
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
-            name="b_86965ed020b63caf48b05c566_f534f52ed1" tabindex="-1" value=""/></div>
-        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-            class="button" /></div>
-      </div>
-    </form>
-  </div>
+      </form>
+    </div>
   );
 }
 
